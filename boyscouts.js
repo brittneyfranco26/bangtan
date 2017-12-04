18758fbbfec881568b62b627060c981cb728fe8d
brittneyfranco26 <brittneyfranco26@users.noreply.github.com>
2017-12-04 14:31:24 -0800

    Create boyscouts.js

---
+++ boyscouts.js
@@ -0,0 +1,14 @@
+function addSuga(elem){
+	document.getElementById("textbox").classList.toggle("suga");
+	elem.innerHTML = "Suga";
+}
+function addV(elem){
+	document.getElementById("textbox").classList.toggle("V");
+	elem.innerHTML= "V"
+}
+function playSuga(elem){
+	document.getElementById("first-love").play();
+}
+function playV(elem){
+	document.getElementById("stigma").play();
+}
