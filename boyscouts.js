c81cd370b64a2281f45787ec0b32e7635277cdb7
brittneyfranco26 <brittneyfranco26@users.noreply.github.com>
2017-12-06 21:50:21 -0800

    Update boyscouts.js

--- boyscouts.js
+++ boyscouts.js
@@ -6,9 +6,28 @@ function addV(elem){
 	document.getElementById("textbox").classList.toggle("V");
 	elem.innerHTML= "V"
 }
+function addKookie(elem){
+	document.getElementById("textbox").classList.toggle("jk"
+}
 function playSuga(elem){
 	document.getElementById("first-love").play();
+	document.getElementById("stigma").pause();	
+}
+function pauseSuga(elem){
+	document.getElementById("first-love").pause()
 }
 function playV(elem){
 	document.getElementById("stigma").play();
+	document.getElementById("first-love").pause();
+}
+function pauseV(elem){
+	document.getElementById("stigma").pause()
+}
+function playKookie(elem){
+	documen.getElementById("begin").play()
+	document.getElementById("stigma").pause()
+	document.getElementById("first-love").pause()
+}
+function pauseKookie(elem){
+	documen.getElementById("begin").play()
 }
